e6310117474d6536d0f9226689a21486a2b8f21f
Aline Rocha <ap_rocha98@hotmail.com>
2024-11-10 14:28:47 -0300

    Configurando tela de login/cadastro

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+ mix.js('resources/js/app.js', 'public/js')
+ .babelConfig({
+     presets: ['@babel/preset-env']
+ })
+ .postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+])
+.version();
+   
